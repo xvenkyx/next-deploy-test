4294043d17f53f1a3b664a512e17f8a5a12bc0e6
xvenkyx <venkyroxx99@gmail.com>
2024-08-27 16:31:27 +0530

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Ok venky</h1>
+      <h1>Test App</h1>
       <a href="/api/users">All users</a>
       <p>Or try to hit /users/api manually</p>
     </main>
